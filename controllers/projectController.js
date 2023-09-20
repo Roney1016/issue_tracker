362b3aa2f9d26c0616f258b1dc9ef9ec09e5ac35
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-09-20 15:31:43 +0530

    add controller create project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,3 +1,17 @@
+const Project = require('../models/project');
+
 module.exports.projectForm = function(req,res){
     return res.render('project_form',{title:'Project form'})
+}
+// create new project
+module.exports.create = async function(req,res){
+   try{
+    console.log(req.body)
+    await Project.create(req.body);
+
+    return res.redirect('/')
+   }catch(error){
+    console.log(error)
+    return res.status(500).send('Internal server Error')
+   }
 }
--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const controller = require('../controllers/projectController')
 const router = express.Router();
-
+//render the create project page
 router.get('/create-form',controller.projectForm)
 
+router.post('/create',controller.create)
 
 
 module.exports = router;
